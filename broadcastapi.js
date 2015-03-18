6f6ebe599f33f6ac1d743fe630af442c8b911f98
KiYugadgeter <zyouhousikaku@gmail.com>
2015-03-18 16:23:08 +0900

    Add Demo

---
+++ broadcastapi.js
@@ -0,0 +1,17 @@
+var input = document.getElementsByTagName("input")[0];
+var hello = document.getElementById("hello");
+var bro = new BroadcastChannel("channel1");
+var sendbutton = document.getElementById("sendbutton");
+bro.onmessage = function (ev) {
+    hello.innerHTML = ev.data;
+    console.log(ev);
+    console.log(ev.data);
+}
+
+sendbutton.onclick = function (e) {
+    var v = input.value;
+    var myobj = {};
+    myobj[v] = Date()
+    bro.postMessage(myobj);
+
+}
